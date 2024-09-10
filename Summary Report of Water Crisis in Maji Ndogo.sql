eff5091f94ad7acc3170b5ddee3835fde65376b6
Miriam Ndukwu <86110856+brain-storm-tech@users.noreply.github.com>
2024-09-10 02:53:38 +0100

    SUMMARY REPORT OF MAJI NDOGO WATER CRISIS

---
+++ Summary Report of Water Crisis in Maji Ndogo.sql
@@ -0,0 +1,41 @@
+/*SUMMARY REPORT OF MAJI NDOGO WATER CRISIS
+1. Most water sources are rural in Maji Ndogo.
+2. 43% of our people are using shared taps. 2000 people often share one tap.
+3. 31% of our population has water infrastructure in their homes, but within that group,
+4. 45% face non-functional systems due to issues with pipes, pumps, and reservoirs. Towns like Amina, the rural parts of Amanzi, and a couple
+of towns across Akatsi and Hawassa have broken infrastructure.
+5. 18% of our people are using wells of which, but within that, only 28% are clean. These are mostly in Hawassa, Kilimani and Akatsi.
+6. Our citizens often face long wait times for water, averaging more than 120 minutes:
+• Queues are very long on Saturdays.
+• Queues are longer in the mornings and evenings.
+• Wednesdays and Sundays have the shortest queues.
+
+RECOMMENDATIONS
+We want to focus our efforts on improving the water sources that affect the most people.
+1. Most people will benefit if we improve the shared taps first.
+2. Wells are a good source of water, but many are contaminated. Fixing this will benefit a lot of people.
+3. Fixing existing infrastructure will help many people. If they have running water again, they won't have to queue, thereby shorting queue times
+for others. So we can solve two problems at once.
+4. Installing taps in homes will stretch our resources too thin, so for now if the queue times are low, we won't improve that source.
+5. Most water sources are in rural areas. We need to ensure our teams know this as this means they will have to make these repairs/upgrades in
+rural areas where road conditions, supplies, and labour are harder challenges to overcome.
+
+PRACTICAL SOLUTIONS:
+1. If communities are using rivers, we will dispatch trucks to those regions to provide water temporarily in the short term, while we send out
+crews to drill for wells, providing a more permanent solution. Sokoto is the first province we will target.
+2. If communities are using wells, we will install filters to purify the water. For chemically polluted wells, we can install reverse osmosis (RO)
+filters, and for wells with biological contamination, we can install UV filters that kill microorganisms - but we should install RO filters too. In
+the long term, we must figure out why these sources are polluted.
+3. For shared taps, in the short term, we can send additional water tankers to the busiest taps, on the busiest days. We can use the queue time
+pivot table we made to send tankers at the busiest times. Meanwhile, we can start the work on installing extra taps where they are needed.
+According to UN standards, the maximum acceptable wait time for water is 30 minutes. With this in mind, our aim is to install taps to get
+queue times below 30 min. Towns like Bello, Abidjan and Zuri have a lot of people using shared taps, so we will send out teams to those
+towns first.
+4. Shared taps with short queue times (< 30 min) represent a logistical challenge to further reduce waiting times. The most effective solution,
+installing taps in homes, is resource-intensive and better suited as a long-term goal.
+5. Addressing broken infrastructure offers a significant impact even with just a single intervention. It is expensive to fix, but so many people can
+benefit from repairing one facility. For example, fixing a reservoir or pipe that multiple taps are connected to. We identified towns like Amina,
+Lusaka, Zuri, Djenne and rural parts of Amanzi seem to be good places to start.
+
+N.B Project_progress tables address the improvements recommended for each type of water source.
+/*
